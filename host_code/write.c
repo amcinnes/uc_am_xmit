dada528d4f7f12d815ab86d07e49a3bfda4a9ffa
Angus McInnes <angus@amcinnes.info>
2012-09-03 19:33:21 +1000

    Made host code isochronous

--- host_code/write.c
+++ host_code/write.c
@@ -5,10 +5,17 @@
 #include <string.h>
 #include <sys/time.h>
 
-// I want about 100ms total buffer, at 1MB/s
-// so each of the two buffers should be about 50KB
-#define BUFSIZE 51200
-#define NUMBUFS 2
+// Total buffer: NUMTRANSFERS*PACKETS*PACKETSIZE*8
+// = 200000 bits = 100ms at 2Mbps
+#define NUMTRANSFERS 2
+#define PACKETS 50
+#define PACKETSIZE 250
+
+double dtime() {
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	return tv.tv_sec + 1e-6 * tv.tv_usec;
+}
 
 void transfer(struct libusb_transfer *x) {
 	/*
@@ -24,16 +31,18 @@ void transfer(struct libusb_transfer *x) {
 	x->length = r;
 	*/
 
-	x->length = BUFSIZE;
-	memset(x->buffer,0x55,x->length);
+	x->length = PACKETS*PACKETSIZE;
+	memset(x->buffer,0xaa,x->length);
+	/*
+	int i;
+	for (i=0;i<BUFSIZE;i+=64) {
+		int f=(i>=BUFSIZE/2);
+		x->buffer[i] = 0x02;
+		x->buffer[i+63] = 0x40;
+	}
+	*/
 
 	libusb_submit_transfer(x);
-}
-
-double dtime() {
-	struct timeval tv;
-	gettimeofday(&tv,NULL);
-	return tv.tv_sec + 1e-6 * tv.tv_usec;
 }
 
 double t;
@@ -44,29 +53,28 @@ void callback(struct libusb_transfer *xfr) {
 		fprintf(stderr,"Transfer failed: %d\n",xfr->status);
 		exit(1);
 	}
-	assert(xfr->actual_length==xfr->length);
 	transfer(xfr);
 
-	total += xfr->actual_length;
+	total += xfr->length;
 	double rate = total/(dtime()-t);
 	fprintf(stderr,"%10d %lf\n",total,rate);
 }
 
 int main() {
-	struct libusb_transfer *xfrs[NUMBUFS];
+	struct libusb_transfer *xfrs[NUMTRANSFERS];
 
 	libusb_init(NULL);
 	libusb_device_handle *dev = libusb_open_device_with_vid_pid(NULL,0x03eb,0x204f);
 	assert(dev);
 
-	t = dtime();
-
 	int i;
-	for (i=0;i<NUMBUFS;i++) {
-		xfrs[i] = libusb_alloc_transfer(0);
-		char *buf = malloc(BUFSIZE);
-		libusb_fill_bulk_transfer(xfrs[i],dev,0x01,buf,0,callback,NULL,0);
+	for (i=0;i<NUMTRANSFERS;i++) {
+		xfrs[i] = libusb_alloc_transfer(50);
+		char *buf = malloc(PACKETS*PACKETSIZE);
+		libusb_fill_iso_transfer(xfrs[i],dev,0x01,buf,PACKETS*PACKETSIZE,PACKETS,callback,NULL,0);
+		libusb_set_iso_packet_lengths(xfrs[i],PACKETSIZE);
 		transfer(xfrs[i]);
 	}
+	t = dtime();
 	while (1) libusb_handle_events(NULL);
 }
